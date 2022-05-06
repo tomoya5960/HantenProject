4ec821b6801c1f5a3c756fc6949d11adfc26f98a
min <>
2022-05-06 18:04:14 +0900

    　

---
+++ Assets/User/RumiRumi/TileData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileData : MonoBehaviour
+{
+    public int _mapLine;        //タイルの配列座標を格納
+    public int _mapColumn;  //タイルの配列座標を格納
+    public bool _isTurnOver;  //反転
+}
